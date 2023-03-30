db275a784d724832b3e9f03123d7613f9261d007
BillArmsty <armsty9@gmail.com>
2023-03-30 12:16:29 +0300

    Functions destructuring

---
+++ functions/target/debug/deps/functions-a46ba137631690b8.d
@@ -0,0 +1,5 @@
+/home/armsty/Coding Projects/hello_cargo/functions/target/debug/deps/functions-a46ba137631690b8: src/main.rs
+
+/home/armsty/Coding Projects/hello_cargo/functions/target/debug/deps/functions-a46ba137631690b8.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/armsty/Coding\ Projects/hello_cargo/functions/target/debug/functions: /home/armsty/Coding\ Projects/hello_cargo/functions/src/main.rs

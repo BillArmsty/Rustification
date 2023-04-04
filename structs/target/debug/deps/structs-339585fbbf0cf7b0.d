631974e846fa41f54bfabd9015f2e4d27acbfc5b
BillArmsty <armsty9@gmail.com>
2023-04-04 19:58:34 +0300

    structs implementation

---
+++ structs/target/debug/deps/structs-339585fbbf0cf7b0.d
@@ -0,0 +1,5 @@
+/home/armsty/Coding Projects/hello_cargo/structs/target/debug/deps/structs-339585fbbf0cf7b0: src/main.rs
+
+/home/armsty/Coding Projects/hello_cargo/structs/target/debug/deps/structs-339585fbbf0cf7b0.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/armsty/Coding\ Projects/hello_cargo/structs/target/debug/structs: /home/armsty/Coding\ Projects/hello_cargo/structs/src/main.rs

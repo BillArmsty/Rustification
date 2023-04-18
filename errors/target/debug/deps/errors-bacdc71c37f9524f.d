da0d930d85db80aa5b6d50b2c88b0957b6a611b9
BillArmsty <armsty9@gmail.com>
2023-04-18 14:44:57 +0300

    details of calling panic! or returning result

---
+++ errors/target/debug/deps/errors-bacdc71c37f9524f.d
@@ -0,0 +1,5 @@
+/home/armsty/Coding Projects/hello_cargo/errors/target/debug/deps/errors-bacdc71c37f9524f: src/main.rs
+
+/home/armsty/Coding Projects/hello_cargo/errors/target/debug/deps/errors-bacdc71c37f9524f.d: src/main.rs
+
+src/main.rs:
---
+++ errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+/home/armsty/Coding\ Projects/hello_cargo/errors/target/debug/errors: /home/armsty/Coding\ Projects/hello_cargo/errors/src/main.rs

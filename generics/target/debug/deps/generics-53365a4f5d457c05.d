a5bd9b216841f9c42eceb5b4a1f46421dd1f970c
Bill Armstrong <armsty9@gmail.com>
2023-04-20 13:04:59 +0300

    Use of generics to reduce code duplication

---
+++ generics/target/debug/deps/generics-53365a4f5d457c05.d
@@ -0,0 +1,5 @@
+/home/bill/Coding Projects/Rustification/generics/target/debug/deps/generics-53365a4f5d457c05: src/main.rs
+
+/home/bill/Coding Projects/Rustification/generics/target/debug/deps/generics-53365a4f5d457c05.d: src/main.rs
+
+src/main.rs:
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+/home/bill/Coding\ Projects/Rustification/generics/target/debug/generics: /home/bill/Coding\ Projects/Rustification/generics/src/main.rs

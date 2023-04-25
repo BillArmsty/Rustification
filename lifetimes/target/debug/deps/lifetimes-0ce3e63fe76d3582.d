71457ce4b02b51a8c9d1a26291cfa66b70357755
Bill Armstrong <armsty9@gmail.com>
2023-04-25 13:19:36 +0300

    Lifetime annotations to ensure there's no dangling references

---
+++ lifetimes/target/debug/deps/lifetimes-0ce3e63fe76d3582.d
@@ -0,0 +1,5 @@
+/home/bill/Coding Projects/Rustification/lifetimes/target/debug/deps/lifetimes-0ce3e63fe76d3582: src/main.rs
+
+/home/bill/Coding Projects/Rustification/lifetimes/target/debug/deps/lifetimes-0ce3e63fe76d3582.d: src/main.rs
+
+src/main.rs:
---
+++ lifetimes/target/debug/lifetimes.d
@@ -0,0 +1,1 @@
+/home/bill/Coding\ Projects/Rustification/lifetimes/target/debug/lifetimes: /home/bill/Coding\ Projects/Rustification/lifetimes/src/main.rs

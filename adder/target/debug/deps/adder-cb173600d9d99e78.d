4909befbce6ea80d110d8d8a4858cbdbb03e31e0
Bill Armstrong <armsty9@gmail.com>
2023-04-26 12:15:47 +0300

    testing features provide a way to specify how code should function to ensure it continues to work as expected

---
+++ adder/target/debug/deps/adder-cb173600d9d99e78.d
@@ -0,0 +1,7 @@
+/home/bill/Coding Projects/Rustification/adder/target/debug/deps/adder-cb173600d9d99e78.rmeta: src/lib.rs
+
+/home/bill/Coding Projects/Rustification/adder/target/debug/deps/libadder-cb173600d9d99e78.rlib: src/lib.rs
+
+/home/bill/Coding Projects/Rustification/adder/target/debug/deps/adder-cb173600d9d99e78.d: src/lib.rs
+
+src/lib.rs:

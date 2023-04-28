fc0d77cf9ed3cbb96424ca34fca575feb19a4780
Bill Armstrong <armsty9@gmail.com>
2023-04-28 17:10:43 +0300

    test driven functionality

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,29 @@
 use std::error::Error;
 use std::fs;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
 pub struct Config {
     pub query: String,
     pub file_path: String,
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
 
 impl Config {
@@ -26,7 +46,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read the contents of the file specified by the second argument
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    let contents = String::from_utf8_lossy(&contents);
+    for line in contents.lines() {
+        if line.contains(&config.query) {
+            println!("{}", line);
+        }
+    }
 
     Ok(())
 }
@@ -34,6 +59,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct OtherConfig {
     pub query: String,
     pub file_path: String,
+}
+pub fn read_file_contents(filename: &str) -> Result<String, std::io::Error> {
+    let contents = fs::read(filename)?;
+    let contents = contents.as_slice();
+    let contents = String::from_utf8_lossy(contents).into_owned();
+    Ok(contents)
 }
 
 impl OtherConfig {

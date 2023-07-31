a8e96b25d1e2a29821bc16b61da889165517eb4a
Bill Armstrong <armsty9@gmail.com>
2023-07-31 13:30:35 +0300

    Created new thread to print one thing while main thread prints sth else

---
+++ closures/target/debug/deps/closures-0c49d0cdf6306ec4.d
@@ -0,0 +1,5 @@
+/home/bill/Coding Projects/Rustification/closures/target/debug/deps/closures-0c49d0cdf6306ec4: src/main.rs
+
+/home/bill/Coding Projects/Rustification/closures/target/debug/deps/closures-0c49d0cdf6306ec4.d: src/main.rs
+
+src/main.rs:
---
+++ threads/target/debug/threads.d
@@ -0,0 +1,1 @@
+/home/bill/Coding\ Projects/Rustification/threads/target/debug/threads: /home/bill/Coding\ Projects/Rustification/threads/src/main.rs
